15d1661a29d6ce857118d41c723a190208d8e91f
Luis Jaco <lmario329@gmail.com>
2025-04-03 18:08:49 -0400

    Updated GetIP.java. Found bug when testing on school Wi-Fi.

--- src/main/java/com/github/luisjaco/GetIP.java
+++ src/main/java/com/github/luisjaco/GetIP.java
@@ -1,25 +1,15 @@
 package com.github.luisjaco;
 
 import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.util.Enumeration;
+import java.net.UnknownHostException;
 
 public class GetIP {
-    // you can quickly run this class to find your ip if you want to test non-local server connections.
     public static void main(String[] args) {
         try {
-            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-            while (interfaces.hasMoreElements()) {
-                NetworkInterface ni = interfaces.nextElement();
-                Enumeration<InetAddress> addresses = ni.getInetAddresses();
-                while (addresses.hasMoreElements()) {
-                    InetAddress addr = addresses.nextElement();
-                    if (!addr.isLoopbackAddress() && addr.isSiteLocalAddress()) {
-                        System.out.println("Possible IP: " + addr.getHostAddress());
-                    }
-                }
-            }
-        } catch (Exception e) {
+            InetAddress localHost = InetAddress.getLocalHost();
+            System.out.println("Local IP Address: " + localHost.getHostAddress());
+        } catch (UnknownHostException e) {
+            System.err.println("Could not determine local IP address.");
             e.printStackTrace();
         }
     }
